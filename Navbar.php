2ef224523fe70e61a4d7973e3870795e20e3b621
Brett <58530824+Bearium-BB@users.noreply.github.com>
2023-04-12 09:36:51 -0500

    teste
    etss

--- Navbar.php
+++ Navbar.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <div class="nav-bar">
     <div class="nav-left">
-        <img id="the" src="/php/BearTube/Img/BearTube Logo.png" alt="Logo">
+        <img id="the" src="Img/BearTube Logo.png" alt="Logo">
     </div>
     <div class="nav-middle">
         <a href="/php/BearTube/Index.php">Home</a>
@@ -9,16 +9,16 @@
         <a href="#">Contact</a>
         <?php
             if (isset($_SESSION['Username'])) {
-                echo '<a href="/php/BearTube/UploadVideo/UploadFile.php">Upload</a>';
+                echo '<a href="UploadVideo/UploadFile.php">Upload</a>';
             }  
         ?>
     </div>
     <div class="nav-right">
         <?php
             if (isset($_SESSION['loggedIn'])) {
-                echo '<a class="sign-in" href="/php/BearTube/AccountManager/AccountManager.php">Account Manager</a>';
+                echo '<a class="sign-in" href="AccountManager/AccountManager.php">Account Manager</a>';
             } else {
-                echo '<a class="sign-in" href="/php/BearTube/SignIn/SignIn.php">Sign In</a>';
+                echo '<a class="sign-in" href="SignIn/SignIn.php">Sign In</a>';
             }
         
         ?>
